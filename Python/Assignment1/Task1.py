6495ea77f934cd67b6384e581bc306ed977feb19
amol.garole <amol.garole@gmail.com>
2025-12-19 11:11:43 +0530

    Initial commit of Assignment 1 tasks

---
+++ Python/Assignment1/Task1.py
@@ -0,0 +1,10 @@
+#Following code accepts 2 nubmers from the user and performs basic arithmetic operations
+
+number1 = float(input("Enter first number: "))
+number2 = float(input("Enter second number: ")) 
+
+print("Addtion: ", number1 + number2)
+print("Subtraction: ", number1 - number2)   
+print("Multiplication: ", number1 * number2)
+print("Division: ", number1 / number2)  
+
